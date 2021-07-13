125176a16e258f4d46fb9afc79f93ccb379e387b
Marcus Florentin <marcus@osmoze.xyz>
2021-07-14 01:57:14 +0200

    Adding Firebase configuration function

--- Sources/SpotrLogic/Firebase/Firebase.swift
+++ Sources/SpotrLogic/Firebase/Firebase.swift
@@ -5,7 +5,14 @@
 //  Created by Marcus on 29/06/2021.
 //
 
+import Firebase
 import FirebaseFirestore
 import FirebaseFirestoreSwift
+
+/// Call this fontion to configure _Firebase_
+public func confirgureFirebase() -> Void {
+
+    FirebaseApp.configure()
+}
 
 let firestore : Firestore = Firestore.firestore()
