afe8bc9e892248257de4823f42c76683728169e0
Marcus Florentin <marcus@osmoze.xyz>
2021-08-30 15:45:39 +0200

    Adding TagGrid Tag

--- Sources/SpotrLogic/Queries/TagGrid.swift
+++ Sources/SpotrLogic/Queries/TagGrid.swift
@@ -14,9 +14,32 @@ public struct TagGrid: Identifiable, Codable, Hashable {
 
     public let id: String?
 
-    public let tags: [Tag]
+    public let tags: [TagGrid.Tag]
 
     // MARK: - TagGrids
+
+
+    // MARK: - Tag
+
+    public struct Tag: Identifiable, Codable, Hashable {
+
+        public typealias ID = String
+
+        public var id: String
+
+        public let name: String
+        public let index: Int?
+        public let picture: Picture?
+
+
+        public init(id: String, name: String, picture: Picture? = nil) {
+            self.id = id
+            self.name = name
+            self.index = nil
+            self.picture = picture
+        }
+    }
+
 
     // MARK: Collection
 
