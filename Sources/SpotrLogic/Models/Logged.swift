cdb063305c10ea88afe799629d78082a6d2cb4c2
Johann Petzold <johann.petzold@gmail.com>
2022-01-06 10:26:25 +0100

    LoggedUser status is now public

--- Sources/SpotrLogic/Models/Logged.swift
+++ Sources/SpotrLogic/Models/Logged.swift
@@ -16,14 +16,14 @@ public class LoggedUser: ObservableObject, Codable {
     @Published public internal(set) var publicMetadata: User? = nil
 
     /// The user status (type).
-    enum Status {
+    public enum Status {
         /// A Free `User` is a user without a `SpotrPro` subscription.
         case free
         /// A `Pro User` is an user who unlock all premium services of Spotr.
         case pro
     }
     
-    var status : Status? {
+    public var status : Status? {
         guard let metadata = privateMetadata else { return nil }
 
         guard let latestSubscription = metadata.billing?.latestSubscription else { return .free }
