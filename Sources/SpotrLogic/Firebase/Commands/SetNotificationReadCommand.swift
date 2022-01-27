914ff6d724077cc0162f09a5d4b9f0adfa6a25b4
Johann Petzold <johann.petzold@gmail.com>
2022-01-27 20:16:44 +0100

    Fixing NotificationReadCommand

--- Sources/SpotrLogic/Firebase/Commands/SetNotificationReadCommand.swift
+++ Sources/SpotrLogic/Firebase/Commands/SetNotificationReadCommand.swift
@@ -21,6 +21,13 @@ struct SetNotificationReadCommand: Encodable {
     let version: String = "1.0.0"
     let payload: Payload
     
+    var id: String = "\(Date().timeIntervalSince1970)_\(UUID().uuidString)"
+    var timestamp: Timestamp = Timestamp(date: Date())
+    var trace_id: String = UUID().uuidString
+    var event_id: String? = "null"
+    var origin: String = "ios"
+    var origin_version: Int? = 144
+    
     init(user_id: String) {
         self.payload = Payload(user_id: user_id)
     }
