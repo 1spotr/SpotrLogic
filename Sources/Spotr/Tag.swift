e23fb09e861e11258edbdf3932c9f62f0c245ef9
Marcus Florentin <marcus@osmoze.xyz>
2022-05-03 04:09:33 +0200

    Moving init on correct model

--- Sources/Spotr/Tag.swift
+++ Sources/Spotr/Tag.swift
@@ -9,6 +9,15 @@ import Foundation
 
 public struct Tag: Identifiable, Codable, Hashable {
 
+				public init(id: String, name: String, childrenIDs: [Tag.ID]?, parentsIDs: [Tag.ID]?, relativesIDs: [Tag.ID]?, siblingsIDs: [Tag.ID]?) {
+								self.id = id
+								self.name = name
+								self.childrenIDs = childrenIDs
+								self.parentsIDs = parentsIDs
+								self.relativesIDs = relativesIDs
+								self.siblingsIDs = siblingsIDs
+				}
+
 				public typealias ID = String
 
 				public let id: ID
--- Sources/SpotrLogic/Models/Tag.swift
+++ Sources/SpotrLogic/Models/Tag.swift
@@ -12,15 +12,6 @@ import FirebaseFirestoreSwift
 // MARK: - Tag
 
 public struct Tag: Identifiable, Codable, Hashable {
-				public init(id: String, name: String, childrenIDs: [Tag.ID]?, parentsIDs: [Tag.ID]?, relativesIDs: [Tag.ID]?, siblingsIDs: [Tag.ID]?) {
-								self.id = id
-								self.name = name
-								self.childrenIDs = childrenIDs
-								self.parentsIDs = parentsIDs
-								self.relativesIDs = relativesIDs
-								self.siblingsIDs = siblingsIDs
-				}
-
 
     public typealias ID = String
 
