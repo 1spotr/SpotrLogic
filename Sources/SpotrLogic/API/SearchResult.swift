ab9c8d9a1dbb8bb542f6e5d1a9ffd7b1f5c5c270
Marcus Florentin <marcus@osmoze.xyz>
2022-06-23 05:59:37 +0200

    Adding fireabase id

--- Sources/SpotrLogic/API/SearchResult.swift
+++ Sources/SpotrLogic/API/SearchResult.swift
@@ -8,13 +8,14 @@
 import Foundation
 
 public struct SearchResult: Codable, Identifiable, Hashable {
-				public init(id: SearchResult.ID, hit: SearchResult.Hit, name: String, values: Int?, news: Int?, thumbnail: URL?) {
+				public init(id: SearchResult.ID, hit: SearchResult.Hit, name: String, values: Int?, news: Int?, thumbnail: URL?, firebaseId: String?) {
 								self.id = id
 								self.hit = hit
 								self.name = name
 								self.values = values
 								self.news = news
 								self.thumbnail = thumbnail
+								self.firebaseId = firebaseId
 				}
 
 				public typealias ID = String
@@ -33,4 +34,5 @@ public struct SearchResult: Codable, Identifiable, Hashable {
 				public let values : Int?
 				public let news : Int?
 				public let thumbnail : URL?
+				public let firebaseId: String?
 }
